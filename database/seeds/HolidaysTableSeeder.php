8f9c4113a86f665531721631a2062288eccffca9
Giuseppe <giuseppecantafio@hotmail.it>
2022-06-14 16:21:45 +0200

    aggiunto db con faker

--- database/seeds/HolidaysTableSeeder.php
+++ database/seeds/HolidaysTableSeeder.php
@@ -11,12 +11,18 @@ class HolidaysTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
+        for($i=0; $i<20; $i++) {
         $newHoliday= new Holiday();
-
-        $newHoliday->title = $faker->company();
-
+        $newHoliday->trip_name = $faker->sentence(2);
+        $newHoliday->country = $faker->country();
+        $newHoliday->city = $faker->city();
+        $newHoliday->days = $faker->numberBetween(0, 20);
+        $newHoliday->departue = $faker->dateTimeInInterval('now', '+3 weeks');
+        $newHoliday->return = $faker->dateTimeInInterval('+2 weeks', '+5 weeks');
+        $newHoliday->description = $faker->paragraph(5, true);
         $newHoliday->save();
+         }
     }
 }
